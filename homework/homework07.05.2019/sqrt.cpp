32c01b4f14492c96169642aa8ef51ec58f478c93
SerjArturich <serjarturich@mail.ru>
2019-05-16 14:49:21 +0400

    armat verjnakan

--- homework/homework07.05.2019/sqrt.cpp
+++ homework/homework07.05.2019/sqrt.cpp
@@ -1,61 +1,32 @@
 #include <iostream>
 using namespace std;
 #define epsilion 0.0000001
-void my_sqrt(int number){
-	int low = 0;
-	int high = number;
-	int root =(low + high) / 2;
-	if (number > 0){
-	while ( high - low > 1){
-		if (root * root <= number){
-			low = root;
-		}
-		else{
-			high = root;
-		
-		}
-		root = (low + high) / 2;
-	}
-	
-	cout << "Root of number is: " <<  root;
-	}
-	else{
-		cout << "In negative number cann't give sqrt";
-	}
-}
 
-void my_sqrt(double number){
-	if(number >= 1){
-        double low = 0;
-        double high = number;
-        double root = 0;
-        while ( high - low > epsilion){
-		root = high - (high - low) / 2;
-                if (root * root > number){
-                        high = root;
-                }
-		else{
-			low = root;
-		}
-	}
-	cout << "Root of number is: " << root;	
-	}
-	else if(number < 0){
-		 cout << "In negative number cann't give sqrt";
-	}
-	else{} 
-}
+double my_sqrt(double number)
+
+{
+    if (number == 0 || number == 1){
+        return number;
+    }
+    double low = 0.0;
+    double high = number / 2;
+    do {
+       low = high;
+       high = (low + (number / low)) / 2;
+       } while ((high - low) != 0);
+    return high;
+} 
 int main ()
 {
-	int number;
-	cout << "Please enter a number: ";
-	cin >> number;
-	my_sqrt(number);
-	cout << endl;
-	double number1;
+	double number1 = 0;
 	cout << "Please enter a number: ";
 	cin >> number1;
-	 my_sqrt(number1);
+	if(number1 < 0){
+		cout << "Innegative number cann't give sqrt";
+	}
+	else{
+	 cout << "Root of number is: " <<  my_sqrt(number1) << endl;
+	}
 
 return 0;
 }
